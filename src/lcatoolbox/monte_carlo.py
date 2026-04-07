fcd1ec44ea715c69f7771cfe12dd217e5f7ab385
Jonathan Larochelle <jonathan.larochelle.3@gmail.com>
2026-04-07 14:28:53 +0200

    decouple calculations from the monte carlo function

--- src/lcatoolbox/monte_carlo.py
+++ src/lcatoolbox/monte_carlo.py
@@ -14,6 +14,74 @@ import numpy as np
 
 # import your own module
 
+
+def _calculate_scores(activities: List[bd.backends.proxies.Activity],
+                      impact_categories: List[Tuple[str, str, str, str]],
+                      parameters: Dict[str, float] = {},
+                      use_exchange_distributions: bool = False,
+                      use_parameters_distributions: bool = False) -> Tuple[List[Dict], List[Dict]]:
+    """
+    Compute scores for all activities in activities (demand=1) and all impact_categories.
+    All parameters are set to the values in parameters.
+
+    If parameters are not specified, all parameters are sampled from their distribution
+    (use_parameters_distributions=True) or set to their default values.
+
+    """
+    for param in ProjectParameter.select():
+        if param.formula is not None:
+            if param.name in parameters.keys():
+                warnings.warn(f"Parameter {param.name} is not set because it is defined by the formula "
+                              f"{param.formula}. Set the value of the parameters of the formula instead.")
+            continue
+
+        if param.name in parameters.keys():
+            new_value = parameters[param.name]
+        elif use_parameters_distributions:
+            mc_rng = stats_arrays.MCRandomNumberGenerator(param.data["uncertainty"])
+            new_value = mc_rng.next()[0]
+        else:
+            new_value = param.data["default"]
+
+        ProjectParameter.update(amount=new_value).where(ProjectParameter.name == param.name).execute()
+
+    Group.get(name="project").expire()
+    bd.parameters.recalculate()
+    ActivityParameter.recalculate_exchanges("group")
+
+    demands = {str(act.id): {act.id: 1} for act in activities}
+    method_config = {"impact_categories": impact_categories, }
+    data_objs = bd.get_multilca_data_objs(functional_units=demands,
+                                          method_config=method_config)
+
+    lca = bc.MultiLCA(demands=demands,
+                      method_config=method_config,
+                      data_objs=data_objs,
+                      use_distributions=use_exchange_distributions, )
+    lca.lci()
+    lca.lcia()
+
+    scores = []
+    parameters = []
+
+    for act in activities:
+        row = {"activity": (act["name"], act["location"])}
+        for ic in impact_categories:
+            row[ic] = lca.scores[ic, str(act.id)]
+        scores.append(row)
+
+    for param in ProjectParameter.select():
+        if param.formula is None:
+            param_type = "independent"
+        else:
+            param_type = "dependent"
+        parameters.append({"name": param.name,
+                           "type": param_type,
+                           "value": param.amount})
+
+    return scores, parameters
+
+
 # TODO: support providing arrays of parameters (for use with Saltelli sampling, for example)
 def run_monte_carlo(activities: List[bd.backends.proxies.Activity],
                     impact_categories: List[Tuple[str, str, str, str]],
@@ -33,68 +101,26 @@ def run_monte_carlo(activities: List[bd.backends.proxies.Activity],
     for act in activities:
         background_activities += get_background_activities(act, foreground_db_name)
     background_activities = list(set(background_activities))
-    method_config = {"impact_categories": impact_categories,}
-
-    demands = {str(act.id): {act.id: 1} for act in activities}
-    demands.update({str(bact.id): {bact.id: 1} for bact in background_activities})
-
-    dependent_parameters = []
-    independent_parameters = []
-    for param in ProjectParameter.select():
-        if param.formula is None:
-            independent_parameters.append(param)
-        else:
-            dependent_parameters.append(param)
 
     scores = []
     scores_background = []
     parameters = []
 
     for i in range(n_iterations):
-        # Sample parameters
-        for param in ProjectParameter.select():
-            if param.formula is None:
-                mc_rng = stats_arrays.MCRandomNumberGenerator(param.data["uncertainty"])
-                new_value = mc_rng.next()[0]
-                ProjectParameter.update(amount=new_value).where(ProjectParameter.name == param.name).execute()
-        Group.get(name="project").expire()
-        bd.parameters.recalculate()
-        ActivityParameter.recalculate_exchanges("group")
-
-        data_objs = bd.get_multilca_data_objs(functional_units=demands,
-                                          method_config=method_config)
-
-        lca = bc.MultiLCA(demands=demands,
-                          method_config=method_config,
-                          data_objs=data_objs,
-                          use_distributions=True)
-        lca.lci()
-        lca.lcia()
-
+        scores_i, parameters_i = _calculate_scores(activities + background_activities,
+                                                   impact_categories,
+                                                   use_exchange_distributions=True,
+                                                   use_parameters_distributions=True)
         for act in activities:
-            row = {"iteration": i,
-                   "activity": (act["name"], act["location"])}
-            for ic in impact_categories:
-                row[ic] = lca.scores[ic, str(act.id)]
-            scores.append(row)
-
+            for row in scores_i:
+                if row["activity"] == (act["name"], act["location"]):
+                    scores.append({"iteration": i, **row})
         for bact in background_activities:
-            row = {"iteration": i,
-                   "activity": (bact["name"], bact["location"])}
-            for ic in impact_categories:
-                row[ic] = lca.scores[ic, str(bact.id)]
-            scores_background.append(row)
-
-        for param in ProjectParameter.select():
-            if param.formula is None:
-                param_type = "independent"
-            else:
-                param_type = "dependent"
-            parameters.append({"iteration": i,
-                               "name": param.name,
-                               "type": param_type,
-                               "value": param.amount})
-
+            for row in scores_i:
+                if row["activity"] == (bact["name"], bact["location"]):
+                    scores_background.append({"iteration": i, **row})
+        for row in parameters_i:
+            parameters.append({"iteration": i, **row})
         _print_monte_carlo_progress(i, n_iterations)
 
     scores_df = pd.DataFrame(scores)
